aea4b19fda2b3117c206b1907f333037d5331ab0
andybardagi <andresbardagi2002@gmail.com>
2023-04-29 11:58:32 -0300

    Add map icon

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,13 +1,18 @@
 import React, { useState, useRef } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Map } from "leaflet";
+import L from "leaflet";
+import { BsFillPinFill } from "react-icons/bs";
+import ReactDOMServer from "react-dom/server";
 
-const OpenStreetMap = () => {
+const MapView = () => {
   const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 });
   const ZOOM_LEVEL = 9;
-  const mapRef = useRef<Map>(null);
-
+  const mapRef = useRef<L.Map>(null);
+  const icon = L.divIcon({
+    className: "custom-icon",
+    html: ReactDOMServer.renderToString(<BsFillPinFill className="text-3xl text-red-800"/>),
+  });
   return (
     <div>
       <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
@@ -15,10 +20,16 @@ const OpenStreetMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={{ lat: -4.043477, lng: 39.668205 }}></Marker>
+        <Marker
+          title="Pelotudo"
+          icon={icon}
+          position={{ lat: -4.043477, lng: 39.668205 }}
+        >
+          Pelotudo
+        </Marker>
       </MapContainer>
     </div>
   );
 };
 
-export default OpenStreetMap;
+export default MapView;
