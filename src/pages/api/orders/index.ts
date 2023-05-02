d355d2068815bba8beb0d4fa8739103d4ba53f15
ginomassei <ginomassei@icloud.com>
2023-05-01 23:07:21 -0300

    credit card validation

--- src/pages/api/orders/index.ts
+++ src/pages/api/orders/index.ts
@@ -1,8 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Order } from '@/models/Order';
+import { PaymentType } from '@/models/PaymentMethod';
+import cardValidator from 'card-validator';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   if (req.method === 'POST') {
     console.log('Order checked out', req.body);
+    const order: Order = req.body;
+
+    if (!order) {
+      return res.status(400).json({
+        message: 'No order provided'
+      })
+    }
+
+    if (!order.paymentMethod) {
+      return res.status(400).json({
+        message: 'No payment method provided'
+      })
+    }
+
+    if (order.paymentMethod.name === PaymentType.Card) {
+      if (!order.paymentMethod.card) {
+        return res.status(400).json({
+          message: 'No card provided'
+        })
+      }
+
+      if (!cardValidator.number(order.paymentMethod.card.cardNumber).isValid) {
+        return res.status(400).json({
+          message: 'Invalid card number'
+        })
+      }
+    }
+
+
     return res.status(200).json({
       message: 'Order saved successfully'
     })
